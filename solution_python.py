9dc5a915f6bb521228b67f43fba30fa8b3236ecf
Kopalnik <62620854+Kopalnik@users.noreply.github.com>
2023-08-03 13:47:04 +0200

    Add files via upload

---
+++ solution_python.py
@@ -0,0 +1,17 @@
+
+ageofvisitors = [25, 18, 31, 61, 52, 14, 10, 11, 13, 70, 34, 21]
+numberofkids = []
+numberofadults = []
+numberofretired = []
+for i in ageofvisitors:
+    if (i <= 15):
+        numberofkids.append(i)
+    elif (i >= 16 and i < 60):
+        numberofadults.append(i)
+    else:
+        numberofretired.append(i)
+
+print("Counted number of visitors for category:")
+print("Number of kids:", len(numberofkids))
+print("Number of adults:", len(numberofadults))
+print("Number of retired:", len(numberofretired))
